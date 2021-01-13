ba8023ee9f13aea01246c5ed90914a3fd7ebb382
RushiGhorpade <rishighorpade@gmail.com>
2021-01-13 19:13:06 +0530

    initial commit

---
+++ spring-jasper-report-master/src/main/java/com/javatechie/report/entity/Employee.java
@@ -0,0 +1,56 @@
+package com.javatechie.report.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+@Table(name = "EMPLOYEE_TBL")
+public class Employee {
+    @Id
+    private int id;
+    private String name;
+    private String designation;
+    private double salary;
+    private String doj;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getDoj() {
+		return doj;
+	}
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+    
+}
